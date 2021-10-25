3d60d98d3c78426d3822f29ed162a2c9134600d2
Andre Muench <andre.muench@statworx.com>
2021-10-25 23:58:25 +0200

    Reorder data row for bulk operation

--- pandas_loader/load.py
+++ pandas_loader/load.py
@@ -27,12 +27,27 @@ class PandasBulkDbLoader(AbstractDbLoader):
     def __init__(self, engine, **kwargs):
         self.engine = engine
 
-    def __call__(self, df: DataFrame, table: Table):     
+    def __call__(self, df: DataFrame, table: Table):
+        pdb_table = table
+
+        def _extract_vals(cols, col_map, data_row):
+            print(data_row)
+            for c in cols:
+                ix = col_map.get(c)
+                if ix is not None:
+                    yield data_row[ix]
+                else:
+                    yield None
+
         def insert_bulk(table, conn, keys, data_iter):
+            # reorder data row according to table definition
+            col_map = dict((v,k) for k,v in enumerate(keys))
+            new_iter = (list(_extract_vals([c.name for c in pdb_table.columns], col_map, d)) for d in data_iter)
+
             import ctds
             con = ctds.connect(server=conn.engine.url.host, user=conn.engine.url.username, password=conn.engine.url.password, database=conn.engine.url.database)
             try:
-                n = con.bulk_insert(f"[{table.schema}].[{table.name}]", data_iter)
+                n = con.bulk_insert(f"[{table.schema}].[{table.name}]", new_iter)
                 con.commit()
                 print(n)
             except Exception as e:
